dda265a9a35f07f53eff0dd032939c7628cdc822
Akshin77 <115021049+Akshin77@users.noreply.github.com>
2022-12-24 17:34:50 +0400

    Add portfolio section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
+// navbar-start
 function toggle_button(){
     var element = document.getElementsByClassName("nav_menu-bar")[0]
     element.classList.toggle("active1");
 }
+// navbar-end
+
+// portfolio-start
+const list = document.querySelectorAll('.list')
+const itemBox = document.querySelectorAll('.item-box')
+
+list.forEach((elem)=>{
+    elem.addEventListener('click',function(){
+        list.forEach((elem)=>{
+            elem.classList.remove('active');
+        });
+        this.classList.add('active');
+
+        let value = this.getAttribute('data-filter');
+        itemBox.forEach(element=>{
+            element.classList.add('hide');
+            if(element.getAttribute('data-item') == value || value == "all"){
+                element.classList.remove('hide');
+            }
+        })
+
+    })
+
+})
+// portfolio-end
