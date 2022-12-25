93b6e47286f91e00d05fa36869b4b1413567a0cf
Akshin77 <115021049+Akshin77@users.noreply.github.com>
2022-12-25 14:45:53 +0400

    Add testimonial section

--- js/main.js
+++ js/main.js
@@ -28,3 +28,19 @@ list.forEach((elem)=>{
 
 })
 // portfolio-end
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
