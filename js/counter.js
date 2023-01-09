0adc544303b034831a3770f75579f260829827fb
Akshin77 <115021049+Akshin77@users.noreply.github.com>
2023-01-10 00:44:30 +0400

    Added counterJs

---
+++ js/counter.js
@@ -0,0 +1,30 @@
+var time = 2,
+  cc = 1;
+$(window).scroll(function() {
+  $('#counter').each(function() {
+    var
+      cPos = $(this).offset().top,
+      topWindow = $(window).scrollTop();
+    if (cPos < topWindow + 400) {
+      if (cc < 2) {
+        $(".cnumber").addClass("viz");
+        $('span').each(function() {
+          var
+            i = 1,
+            num = $(this).data('num'),
+            step = 700 * time / num,
+            that = $(this),
+            int = setInterval(function() {
+              if (i <= num) {
+                that.html(i);
+              } else {
+                cc = cc + 2;
+                clearInterval(int);
+              }
+              i++;
+            }, step);
+        });
+      }
+    }
+  });
+});
